a87f8061976ae739f6897acab95f9fa5eee64625
Abhishek Singh <singh4758@gmail.com>
2022-10-22 08:35:52 +0530

    fix: comment issue

--- NativeScript/insertManyUser.sh
+++ NativeScript/insertManyUser.sh
@@ -14,7 +14,7 @@ rm -rf ../dataGenerated
 mkdir ../dataGenerated
 cp /home/abhishek/Documents/Myself/script/staticData/*.json /home/abhishek/Documents/Myself/script/dataGenerated
 
-# mongorestore --drop --archive="../staticData/Native_Driver"
+mongorestore --drop --archive="../staticData/Native_Driver"
 
 gnome-terminal --tab --command="bash -c 'endyarnstart() { exec bash; }; trap endyarnstart INT; cd ../../Native_Driver/; clinic doctor  --collect-only  --dest ../NativeDoctor/insertManyUser/insertManyD${noOfRecords}R${noOfRequest}C${noOfConnection} -- node ./dist/index.js'"
 
